c05f456d5d21a523e2456e44a84f67b298cb25a4
saurabh <palsaurabh64@gmail.com>
2021-08-25 14:02:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Squarecomponents from './Squarecomponents';
+
+const initialState = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
+  
+  const initialState = ["", "", "", "", "", "", "", "", ""];
+
+  const [gameState, updateGameState] = useState(initialState);
+  const [isXChance, updateIsXChance] = useState(false)
+
+  const onSquareClicked = (index) => {
+    let strings = Array.from(gameState);
+    strings[index] = isXChance ? "X" : "0";
+    updateGameState(strings);
+    updateIsXChance(!isXChance);
+    
+  }
+
+  const clearGame=()=>{
+    updateGameState(initialState)
+  }
+
+  useEffect(() => {
+    const winner = checkWinner(); 
+    if (winner) {
+      alert(`${winner} has won the game`);
+      clearGame()
+    }
+  },[gameState])
+
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        return gameState[a];
+      }
+    }
+    return null;
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-header">
+      <p className="heading-text"> ReactJS Tic-Tac-Toe </p>
+
+      <div className="row jc-center">
+        <Squarecomponents className="b-bottom-right" state={gameState[0]} onClick={() => onSquareClicked(0)} />
+        <Squarecomponents className="b-bottom-right" state={gameState[1]} onClick={() => onSquareClicked(1)} />
+        <Squarecomponents className="b-bottom" state={gameState[2]} onClick={() => onSquareClicked(2)} />
+      </div>
+      <div className="row jc-center" >
+        <Squarecomponents className="b-bottom-right" state={gameState[3]} onClick={() => onSquareClicked(3)} />
+        <Squarecomponents className="b-bottom-right" state={gameState[4]} onClick={() => onSquareClicked(4)} />
+        <Squarecomponents className="b-bottom" state={gameState[5]} onClick={() => onSquareClicked(5)} />
+      </div>
+      <div className="row jc-center">
+        <Squarecomponents className=" b-right " state={gameState[6]} onClick={() => onSquareClicked(6)} />
+        <Squarecomponents className=" b-right" state={gameState[7]} onClick={() => onSquareClicked(7)} />
+        <Squarecomponents className="" state={gameState[8]} onClick={() => onSquareClicked(8)} />
+      </div>
+      <button className="clear-button" onClick={clearGame} > Clear Game</button>
+     
     </div>
   );
 }
